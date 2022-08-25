16e08cc2a1d75046cd7fe9a8cbc99402beee4d62
Tuyet-Vy <nhuochanvu@gmail.com>
2022-08-26 02:11:33 +0700

    done set up redux

---
+++ src/redux/configStore.jsx
@@ -0,0 +1,9 @@
+import {combineReducers, createStore} from 'redux';
+
+const rootReducer = combineReducers({
+    number: (state = 1) => {
+        return state;
+    },
+})
+
+export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
